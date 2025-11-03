5ad0733df8461ca6690b116babaef6b7b51e8c1f
gangya from codespaces linkedin <gangya@gmail.com>
2025-11-03 17:35:24 +0000

    Like button feature added

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView 
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,6 +10,38 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Notes
 from .forms import NotesForm
 # Create your views here.
+
+"""
+#This method for updating an specific attribute of the model isn't a good practice
+
+class UpdateLikeNote(LoginRequiredMixin, UpdateView):
+   model = Notes
+   #fields = ["title","text"]
+   #form_class = NotesForm
+   context_object_name = "note"
+   #template_name = "notes/update_note.html"
+   
+   success_url = "notes/display_note_details.html"
+   login_url = "/admin/"
+   
+"""
+"""
+This methos is appropiated to update an attribute of the model  
+"""
+@login_required(login_url="/admin/")
+def UpdateLikeNote(request, pk):
+       #print(request.method)       
+       if request.method == 'POST':
+         try:
+            #note = models.Notes.objects.get(pk=pk)
+            note = get_object_or_404(Notes, pk=pk)
+            note.likes+=1
+            note.save()
+            return HttpResponseRedirect(reverse("DisplayNotesDetailsView", args=(pk,)))
+         except:
+            raise Http404("Notes matching query, note does not exist!")   
+       raise Http404("Only Post method for changing system!", request.method)
+
 
 class DeleteNote(LoginRequiredMixin, DeleteView):
    model = Notes
